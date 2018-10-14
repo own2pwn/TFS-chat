42e2465c734d439243430124c17d0ad693d8b666
own2pwn <7850039+own2pwn@users.noreply.github.com>
2018-10-14 11:21:20 +0300

    + Bridging header

---
+++ TFS-chat/TFS-chat/Module/Themes/ThemesViewController.h
@@ -0,0 +1,17 @@
+//
+//  ThemesViewController.h
+//  TFS-chat
+//
+//  Created by Evgeniy on 14/10/2018.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ThemesViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TFS-chat/TFS-chat/Module/Themes/ThemesViewController.m
@@ -0,0 +1,21 @@
+//
+//  ThemesViewController.m
+//  TFS-chat
+//
+//  Created by Evgeniy on 14/10/2018.
+//  Copyright © 2018 Evgeniy. All rights reserved.
+//
+
+#import "ThemesViewController.h"
+
+@interface ThemesViewController ()
+
+@end
+
+@implementation ThemesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
